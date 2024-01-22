61ad096f0469afbc655844a6b6510f4ca6974521
KANIKHA M <mkanikha@gmail.com>
2024-01-22 08:52:04 +0530

    javascript first commit

---
+++ operators.js
@@ -0,0 +1,34 @@
+/*//assignments operators
+var a=10
+console.log("assignment operator:",a)
+//arithmetic operator
+var aA = 10
+var bB = 10
+console.log("arithmetic operator:",aA+bB)
+console.log(typeof aA)
+console.log("arithmetic operator:",aA-bB)
+console.log("arithmetic operator:",aA*bB)
+console.log("arithmetic operator:",aA/bB)
+console.log("arithmetic operator:",aA%bB)*/
+
+//logical operator -> && ,|| , ! => output =
+
+var al = 10
+var bl = 20
+console.log("logical operator && output",al&&bl)
+console.log("logical operator || output",al||l)
+console.log("logical operator ! output",!al)
+
+console.log(10||20)
+console.log(0||20)
+console.log(20||0)
+console.log(100||20 && 0)
+console.log(!10)
+console.log(!0)
+console.log(!-10)
+console.log(!+10)
+
+
+
+
+
---
+++ variables.js
@@ -0,0 +1,10 @@
+a='a'
+console.log("general variable declaration a='a':",a)
+var b = 'var'
+console.log("This variable belongs to var type value is",b)
+let c = 'let'
+console.log("This variable belongs to let type value",c)
+const d =100
+console.log("constant variable const d='d'value is",d)
+/*const d =200
+console.log("constant variable const d='d'value is",d)  const can't be update entire program*/
